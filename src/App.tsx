f67f6f0f22fe84770ff0d8c9bba7f1967742b656
Meerim Kerkibasheva <mkerkibasheva@gmail.com>
2023-12-16 13:27:15 +0600

    commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NotesApp from "./components/NotesApp";
+
+const App: FC = () => <NotesApp />;
+
+export default App;
